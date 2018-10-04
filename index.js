f58bd816bc2eb557a45d2b6b5450e1ba585c2e2b
Dimitris Raviolos <dictummortuum339@gmail.com>
2018-10-04 18:41:48 +0300

    olympiad

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const path = "./resources/www.numachi.com/~ccount/hmepa/calendars";
 const olympiad = require('./src/olympiad')(path);
+const suncalc = require('suncalc');
 
 olympiad(700).then(d => console.log(d));
+
+console.log(
+  suncalc.getTimes(new Date(), 37.9838, 23.7275)
+)
--- src/olympiad.js
+++ src/olympiad.js
@@ -4,18 +4,28 @@
  * Returns an Olympiad
  * Which has 4 years
  * Every year may contain an extra Poseideon month
+ * This piece of code determines if a year contains an extra month by
+ * checking the hmera calendar files.
  */
 
 const glob = require('glob-promise');
-const months = require('./months.json')
+const months = require('./months.json');
+
+const month = o => y => name => o + '.' + y + '.' + name + '.html';
 
 const year = years => o => y => {
-  let res = years.filter(d => o + '.' + y + '.' + 'Poseideon-2.html' === d);
-  return res.length === 1 ? months.extra : months.normal;
+  let fn = month(o)(y);
+  let res = years.filter(d => fn('Poseideon-2') === d);
+  let tmp = res.length === 1 ? months.extra : months.normal;
+  return tmp.map(fn);
 }
 
 module.exports = path => async o => {
   let extra = await glob("[0-9]*-*", {cwd: path});
-  let f = year(extra)(o);
-  return [1,2,3,4].map(f);
+  let fn = year(extra)(o);
+  
+  return {
+    olympiad: o + '.html',
+    years: [1,2,3,4].map(fn)
+  };
 }
