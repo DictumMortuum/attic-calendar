082c6cb060f315f419337efc24b70c0a9f631815
DictumMortuum <theio.vrefos@gmail.com>
2018-10-18 00:50:26 +0300

    wip

--- wikipedia.js
+++ wikipedia.js
@@ -1,8 +1,26 @@
 const request = require('request-promise');
+const festivals = require('./src/festivals.json').festivals;
+const festivals_new = {...festivals};
 
 const args = '?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=';
 const url = locale => 'https://' + locale + '.wikipedia.org/w/api.php';
 
-request({
-  uri: url('en') + args + 'Panathenaia'
-}).then(d => console.log(d))
+const search = async term => {
+  let {query: {pages}} = await request({
+    uri: url('en') + args + term,
+    json: true
+  });
+
+  Object.keys(pages).map(o => {
+    let {extract} = pages[o];
+    festivals_new[term].desc = extract;
+    console.log("===============");
+    console.log(festivals_new);
+  });
+}
+
+Object.keys(festivals).map(f => {
+  search(f);
+}).then(d => {
+  console.log(d);
+})
